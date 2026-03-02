cbc22cd4231b59334e09e19d2bfae754c0fda3ae
mat-123 <156803363+Mat-123@users.noreply.github.com>
2026-03-02 16:13:04 +0100

    Layout restyling of product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import "./ProductCard.css";
 
 export default function ProductCard({ product }) {
   const isTaxFree = [1, 2, 3].includes(product.category);
 
   return (
-    <div className="card mb-3" style={{ width: "18rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">{product.name}</h5>
-        <div className="mb-2">
-          {product.isImported && (
-            <span className="badge bg-warning me-1">Imported</span>
-          )}
-          {isTaxFree && <span className="badge bg-success">Tax Free</span>}
-        </div>
-        <p className="card-text">Price: €{product.price.toFixed(2)}</p>
-        <button
-          className="btn btn-sm btn primary"
-          onClick={() => onAddToBasket(product)}
-        >
-          Add To Basket
-        </button>
+    <div className="card position-relative h-100 card-custom">
+      {product.isImported && (
+        <span className="badge bg-warning position-absolute top-0 start-0 m-2 text-dark">
+          Imported
+        </span>
+      )}
+      {isTaxFree && (
+        <span className="badge bg-success position-absolute top-0 end-0 m-2">
+          Tax Free
+        </span>
+      )}
+      <img
+        src="src\media\product-image.jpg"
+        className="card-img-top"
+        alt="product image"
+      />
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title title-min-height">{product.name}</h5>
       </div>
+      <p className="card-text ms-auto px-4">
+        Price: €{product.price.toFixed(2)}
+      </p>
+      <button
+        className="btn btn-sm btn-primary mt-auto mx-2 mb-2"
+        onClick={() => onAddToBasket(product)}
+      >
+        Add To Basket
+      </button>
     </div>
   );
 }
